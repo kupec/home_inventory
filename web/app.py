dfb917930cc42aaa145fa86a10925151e5e22958
Dmitry Kostromin <kupec-k@ya.ru>
2025-03-25 20:35:41 +0300

    Add web

---
+++ web/app.py
@@ -0,0 +1,33 @@
+from fastapi import File, UploadFile, HTTPException, FastAPI
+from fastapi.staticfiles import StaticFiles
+from jinja2 import Environment, PackageLoader, select_autoescape
+from starlette.responses import HTMLResponse
+app = FastAPI()
+
+app.mount("/inventory/static", StaticFiles(directory="static"), name="static")
+
+env = Environment(
+    loader=PackageLoader("app"),
+    autoescape=select_autoescape()
+)
+template = env.get_template("page.html")
+
+@app.get("/inventory/{id}")
+async def page(id: int):
+    return HTMLResponse(template.render(
+        image_src=f'/inventory/static/{id}',
+        upload_url=f'/inventory/{id}/upload',
+    ))
+
+@app.post("/inventory/{id}/upload")
+def upload(id: int, file: UploadFile = File(...)):
+    try:
+        contents = file.file.read()
+        with open(f'static/{id}', 'wb') as f:
+            f.write(contents)
+    except Exception:
+        raise HTTPException(status_code=500, detail='Something went wrong')
+    finally:
+        file.file.close()
+
+    return {"status": "OK"}
